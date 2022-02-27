b325faade9c1b7ce80db681469b514ed4558c8f5
CommandStorm5 <commandstorm5@gmail.com>
2022-02-27 12:49:59 +0100

    Rewrite player input parser and movement handler

--- Server/Server.java
+++ Server/Server.java
@@ -92,12 +92,57 @@ public class Server {
         }
         return output;
     }
-    public static int[] ParseResponse(String response) {
-        String[] coords_bad = response.split(" ");
-        int[] coords = new int[coords_bad.length];
-        for (int i = 0; i < coords.length; i++){
-            coords[i] = Integer.parseInt(coords_bad[i]);
+    public static int[] parseResponse(String response) {
+        String[] inputs_bad = response.split(" ");
+        int[] inputs = new int[inputs_bad.length];
+        for (int i = 0; i < inputs.length; i++){
+            inputs[i] = Integer.parseInt(inputs_bad[i]);
         }
-        return coords;
+        return inputs;
+    }
+    public static void runMovement(int[] inputs) {
+        for (int i = 0; i < inputs.length; i++) {
+            if ((inputs[i] & 0b10000000) != 0 && players[i][0] > 0 && walls[players[i][0]-1][players[i][1]] != 'w') {
+                players[i][0] -= 1;
+                players[i][5] = 1;
+            } else if ((inputs[i] & 0b01000000) != 0 && players[i][0] < size_x-1 && walls[players[i][0]+1][players[i][1]] != 'w') {
+                players[i][0] += 1;
+                players[i][5] = 3;
+            } else if ((inputs[i] & 0b00100000) != 0 && players[i][1] > 0 && walls[players[i][0]][players[i][1]-1] != 'w') {
+                players[i][1] -= 1;
+                players[i][5] = 2;
+            } else if ((inputs[i] & 0b00010000) != 0 && players[i][1] < size_x-1 && walls[players[i][0]][players[i][1]+1] != 'w') {
+                players[i][1] += 1;
+                players[i][5] = 4;
+            }
+            if ((inputs[i] & 0b00001000) != 0 && players[i][2] == 0) {
+                players[i][2] = players[i][5];
+                players[i][3] = players[i][0];
+                players[i][4] = players[i][1];
+            }
+            if (walls[players[i][3]][players[i][4]] == 'w') {
+                players[i][2] = 0;
+            }
+        }
+        if (clock.millis() > then) {
+            then += 20;
+            for (int i = 0; i < players.length; i++) {
+                if (players[i][2] == 1) {
+                    players[i][3] -= 1;
+                } else if (players[i][2] == 2) {
+                    players[i][4] -= 1;
+                } else if (players[i][2] == 3) {
+                    players[i][3] += 1;
+                } else if (players[i][2] == 4) {
+                    players[i][4] += 1;
+                }
+                for (int j = 0; j < players.length; j++) {
+                    if (i != j && players[i][3] == players[j][0] && players[i][4] == players[j][1]) {
+                        players[j][0] = 0;
+                        players[j][1] = 0;
+                    }
+                }
+            }
+        }
     }
 }  
